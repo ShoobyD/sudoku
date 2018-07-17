7f0bd7ee52f18babce6f5c9ca1fe2781b401f8a4
ShoobyD <bmusty@gmail.com>
2018-07-17 23:02:03 +0300

    polyfill and readme

--- sudoku.js
+++ sudoku.js
@@ -1,20 +1,5 @@
 
 /*
- * polyfills
- */
-if ( !Array.prototype.flat ) {
-
-	Array.prototype.flat = function( search, start ) {
-
-		return this.reduce((acc, val) => acc.concat(val), []);
-
-	};
-
-}
-
-
-
-/*
  * constructor Sudoku
  */
 function Sudoku( ELM_SEL, BLOCK_SIZE ) {
