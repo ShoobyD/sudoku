cce4b7eeb2670bf02543308912a89ad25d743f73
ShoobyD <bmusty@gmail.com>
2018-08-01 01:47:22 +0300

    KEYCODES ->utils

--- sudoku.js
+++ sudoku.js
@@ -490,47 +490,6 @@ function Sudoku( ELM_SEL, BLOCK_SIZE ) {
 	const RANGE       = [ ...Array( SIZE ).keys() ];
 	const RANGEALL    = [ ...Array( SIZE * SIZE ).keys() ];
 	const DIGITS      = RANGE.map( i => i + 1 );
-	const KEYCODES    = {
-
-		'BACKSPACE' : 8,
-		'BKSP'      : 8,
-		'DELETE'    : 46,
-		'ESC'       : 27,
-		'ESCAPE'    : 27,
-		'SPACE'     : 32,
-
-		'TAB'       : 9,
-		'ENTER'     : 13,
-
-		'SHIFT'     : 16,
-		'CTRL'      : 17,
-		'CONTROL'   : 17,
-		'ALT'       : 18,
-
-		'C'         : 67,
-		'COPY'      : 67,
-		'V'         : 86,
-		'PASTE'     : 86,
-
-		'PAGEUP'    : 33,
-		'PAGEDOWN'  : 34,
-		'END'       : 35,
-		'HOME'      : 36,
-
-		'F1'        : 112,
-		'F12'       : 123,
-
-		'LEFT'      : 37,
-		'UP'        : 38,
-		'RIGHT'     : 39,
-		'DOWN'      : 40,
-
-		'NUM0'      : 48,
-		'NUM9'      : 57,
-		'NUMPAD0'   : 96,
-		'NUMPAD9'   : 105,
-
-	};
 
 	// DOM shit
 	const $wrap       = $( ELM_SEL );
